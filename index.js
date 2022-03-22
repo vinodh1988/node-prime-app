e7c906dc6561128f8a5b0e3fc0216092c954d3f9
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-22 14:18:37 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require("fs")
+
+fs.readFile("package.json","utf-8",function(err,data){
+  if(err) 
+     console.log(err)
+  else
+     console.log(data)
+})
+
+console.log("Other part of the app")
