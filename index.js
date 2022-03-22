c3fda6f7ba1a730ec9690c22f24125e00568036c
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-22 15:15:36 +0530

    file-serve

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express")
  const app = express()
+ const path =require("path")
 
  app.get("/",function(request,response){
      response.send("Node js is working...!!!")
+ })
+
+ app.get("/home",function(request,response){
+     response.sendFile(path.join(__dirname,'public/index.html'))
  })
 
  app.listen("8000",function(){
