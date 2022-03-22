d31d7a95f5800dda9ee1d25f47a330103c54d724
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-22 15:33:05 +0530

    css-commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
  const app = express()
  const path =require("path")
 
+ app.use(express.static(path.join(__dirname,"public/styles")))
+ app.use(express.static(path.join(__dirname,"public/scripts")))
  app.get("/",function(request,response){
      response.send("Node js is working...!!!")
  })
@@ -9,7 +11,10 @@ const express = require("express")
  app.get("/home",function(request,response){
      response.sendFile(path.join(__dirname,'public/index.html'))
  })
-
+/*
+ app.get("/styles/index.css",function(request,response){
+    response.sendFile(path.join(__dirname,'public/styles/index.css'))
+})*/
  app.listen("8000",function(){
      console.log("server is running on port 8000")
  })
